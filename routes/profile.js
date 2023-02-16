21b515f08924e4bfb868f8518a58f8b01bb8dcf4
GNITOAHC <chaotingchen10@gmail.com>
2023-02-17 00:27:22 +0800

    feat(profile & route): Add like stories to profile and like story route

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Story = require("../models").story;
+const User = require("../models").user;
 // const profileValidation = require("../validation").profileValidation;
 
 router.use((req, res, next) => {
@@ -16,6 +17,41 @@ router.get("/my-stories", async (req, res) => {
     let storyFound = await Story.find({ author: req.user._id }).exec();
     res.send(storyFound);
   } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+router.post("/like-story", async (req, res) => {
+  try {
+    // ObjectId.valueOf() -> Return value of the ObjectID() as a lowercase hexadecimal string.
+    let likedStoryID = req.body.likedStoryID.valueOf();
+    const id = req.user._id;
+    let likeBefore = await User.countDocuments({
+      _id: id,
+      like: { $in: [likedStoryID] },
+    }).exec();
+
+    if (likeBefore === 1) {
+      await User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $pull: { like: likedStoryID } },
+        {
+          returnOriginal: false,
+        }
+      ).exec();
+      return res.send("Like before, unlike success");
+    } else {
+      await User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $push: { like: likedStoryID } },
+        {
+          returnOriginal: false,
+        }
+      ).exec();
+      return res.status(200).send("Like success");
+    }
+  } catch (e) {
+    console.log(e);
     return res.status(500).send(e);
   }
 });
